99500658a2fdf4eb14f25efa6c1661fb7ee8af6f
Duideks <lychjiigrok@gmail.com>
2026-06-01 15:50:29 +0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
     user_surname VARCHAR(30) NULL,
     user_pass VARCHAR(255) NOT NULL,
     user_phone VARCHAR(20) NOT NULL,
-    user_role ENUM('admin','user','guest') DEFAULT 'guest'
+    user_role ENUM('admin','user') DEFAULT 'user'
 );
 
 CREATE TABLE remont_services (
@@ -27,7 +27,7 @@ CREATE TABLE orders (
     order_square INT NOT NULL,
     order_description TEXT,
     order_date DATE NOT NULL,
-    order_status ENUM('new','in_progress','done','cancelled') DEFAULT 'new',
+    order_status ENUM('in_progress','done') DEFAULT 'in_progress',
     order_address VARCHAR(255),
     order_phone VARCHAR(20),
     FOREIGN KEY (order_user_id) REFERENCES users(user_id),
